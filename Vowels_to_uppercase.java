cfa751d45b13df70f0143e2dd187b1185b052bab
Anu <154891466+Anusuya-techie@users.noreply.github.com>
2024-02-18 11:58:07 +0530

    Add files via upload

---
+++ Vowels_to_uppercase.java
@@ -0,0 +1,17 @@
+package tasks;
+
+public class Vowels_to_uppercase {
+
+	public static void main(String[] args) {
+    String name = "alvinas sadhana";
+    System.out.println("original string = " + name);
+    System.out.println("After replacing all vowels into capital case = " + name.replaceAll("a", "A").replaceAll("e", "E") .replaceAll("i", "I").replaceAll("o", "O")
+    .replaceAll("u", "U"));
+		                                      
+		    
+		
+		// TODO Auto-generated method stub
+
+	}
+
+}
